eedaa55abf9c24ca2c6703a30312969357039e40
Andries <nieu0082@hz.nl>
2017-11-01 13:20:45 +0100

    Twee interfaces toegevoegd.

---
+++ Calculator/Calculator2/CalculatorMusthaves.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    public interface CalculatorMusthaves
+    {
+        int Sum(int getal1, int getal2);
+
+        int Diff(int getal1, int getal2);
+
+        int Product(int getal1, int getal2);
+
+        int Quotient(int getal1, int getal2);
+    }
+}
---
+++ Calculator/Calculator2/CalculatorShouldhaves.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    public interface CalculatorShouldhaves
+    {
+        int Rest(int getal1, int getal2);
+
+        int Min(int getal1, int getal2);
+
+        int Max(int getal1, int getal2);
+    }
+}
