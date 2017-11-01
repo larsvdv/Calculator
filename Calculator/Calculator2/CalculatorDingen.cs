6352a4a50ab408a644768f06cba61a9f8b9f91bb
Lars van de Vrede <larsvandevrede@hotmail.com>
2017-11-01 14:19:53 +0100

    Ik heb geen idee wat dit doet

---
+++ Calculator/Calculator2/CalculatorDingen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    class CalculatorDingen : ICalculatorMusthaves
+    {
+        public int Sum(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Diff(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Product(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Quotient(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
